f6045e1b618234836d4a8c357f4373d7d6969342
minju <iaki111@naver.com>
2024-10-28 01:55:44 +0900

    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getAnswer() {
     const userQuestion = document.getElementById("userQuestion").value;
 
     try {
-        let response = await fetch('https://dapper-puppy-530506.netlify.app/get_answer', {
+        let response = await fetch('https://romance-magic-conch.netlify.app/get_answer', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
